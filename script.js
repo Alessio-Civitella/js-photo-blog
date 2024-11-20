a72df52e11bde6e334024700cd896bf2251e02bb
alessio-civitella <alessio.civitella19@gmail.com>
2024-11-20 15:25:39 +0100

    Feat: logica implementata con molta difficoltà :(

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const main = document.querySelector('main'); 
-
+    const main = document.querySelector('main');
+    const overlay = document.querySelector('.overlay');
+    const overlayImage = overlay.querySelector('img');
+    const closeBtn = document.createElement('button');
+    
+    closeBtn.classList.add('close-btn');
+    closeBtn.textContent = 'X';
+    overlay.appendChild(closeBtn);
+    
     axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
         .then(response => {
-            const photos = response.data; 
-            
+            const photos = response.data;
+
             let polaroidsHTML = '';
 
             photos.forEach(photo => {
@@ -17,6 +24,29 @@ document.addEventListener('DOMContentLoaded', () => {
             });
 
             main.innerHTML = polaroidsHTML;
+
+            const cardImg = document.querySelectorAll('.polaroid img');
+            
+            cardImg.forEach((image) => {
+                image.addEventListener('click', () => {
+                    
+                    overlay.style.display = 'flex';
+                    overlayImage.src = image.src;
+                });
+            });
+
+            
+            closeBtn.addEventListener('click', () => {
+                overlay.style.display = 'none';
+            });
+
+            
+            overlay.addEventListener('click', (over) => {
+                if (over.target === overlay) {
+                    overlay.style.display = 'none';
+                }
+            });
+
         })
         .catch(error => {
             console.error("Errore durante il caricamento delle immagini:", error);
